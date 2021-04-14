9f74e1a8c87cab6aad74cd3b71f926ee6ed9245f
Diego Alves <dalves@pagseguro.com>
2021-04-14 11:41:36 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<Button text="Click me!" />
+</template>
+
+<script>
+import Button from '@/components/Button.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Button,
+  }
+}
+</script>
---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="onClick()">{{text}}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+  },
+  setup() {
+    return {
+      onClick: () => {
+        alert("You click me!");
+      },
+    };
+  },
+});
+</script>
